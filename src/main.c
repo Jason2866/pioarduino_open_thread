fe93a9cf7b86201d999a3e020f24608835e24392
rwrozelle <rwrozelle@gmail.com>
2025-05-24 07:47:03 -0400

    Latest Testing

--- src/main.c
+++ src/main.c
@@ -1,10 +1,1 @@
-#include <stdio.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-
-void app_main(void) {
-    while (1) {
-        printf("Hello, ESP-IDF!\n");
-        vTaskDelay(pdMS_TO_TICKS(2000));
-    }
-}
+void app_main() {}
